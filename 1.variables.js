8142273c43a84dce22560e6b05d87adb403850ba
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-21 02:09:43 +0600

    simple about variables

---
+++ 1.variables.js
@@ -0,0 +1,16 @@
+// don't use var, has problem, later it is explained
+let name = "Imam";
+console.log(name);
+
+// if we want 2 variables together
+let firstName, lastname;
+//but modern best practise
+let firstNameMe = "Imam";
+let lastNameMe = "Hossain";
+
+// ## rules ##
+// can't be reserved keyword like if,for
+// should be meanigful
+// can't start with a number
+// can't contain a space or hypen(-)
+// are case sensitive ex- firstName,FirstName are different
