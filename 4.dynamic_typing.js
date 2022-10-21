bf3c314691498bef783f6b65b0a125fffb4d800a
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-22 02:09:56 +0600

    undefined and null are very impoertant

---
+++ 4.dynamic_typing.js
@@ -0,0 +1,8 @@
+// we can change type of variables
+// ## some abnormals to look
+
+let firstName = undefined;
+let selectColor = null;
+
+// here firstName assgined to undefined value
+// but selectColor assigned to null which is diferent from undefined
