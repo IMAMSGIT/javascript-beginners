83e905eb374c9ef174db2271a343885b826428cb
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-21 18:31:51 +0600

    primitive types

---
+++ 3.primitive_types.js
@@ -0,0 +1,8 @@
+// primitive types
+// -String
+// -Number
+// -Boolean
+// -undefined
+// -null
+// ### uninitialized variables will be undefined by default
+// ### we can explcitly assign null to a variable
