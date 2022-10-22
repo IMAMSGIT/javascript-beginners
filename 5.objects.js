cd793fe56dd2f574c9bbed4be475fbf8eafe2ebf
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-22 21:43:36 +0600

    objects

---
+++ 5.objects.js
@@ -0,0 +1,18 @@
+// Object is like a person who has
+// name, age, address
+// it's reference type
+
+// ## the method to create object
+let person = {
+  name: "Imam",
+  age: 45,
+};
+console.log(person);
+
+// there is 2 way to access and change object properties
+
+//Dot notation
+// person.name = "Hossain";
+
+//Bracket Notation
+// person["name"] = "Hossain";
