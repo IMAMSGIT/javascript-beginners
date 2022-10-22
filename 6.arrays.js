a47fdc0cc973e26cf715d6b30702cbbfa9c84517
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-22 23:21:23 +0600

    basic array

---
+++ 6.arrays.js
@@ -0,0 +1,9 @@
+let someColors = ["white", "black"];
+
+//we can assign different data type into the array
+someColors[2] = 1;
+
+console.log(someColors);
+
+// every arrays are object
+//they have properties and functions just all refernece types
