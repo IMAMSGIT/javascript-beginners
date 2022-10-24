fdee880cfa956306e77e8a630ad65429376af271
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-25 00:34:26 +0600

    operator started

---
+++ 9.operators.js
@@ -0,0 +1,8 @@
+// operators used to create expression by varialbes and constants
+// expression helps to build logic and algorithm
+
+// ## JS operators
+// 1) artihmetic 2) assignment 3) comparison 4)logical 5) bitwise
+
+//--arthmetic are simple math operators
+// except %,**, increment and decrement
