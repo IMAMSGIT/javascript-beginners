23cc7c09f6d6fd858a4944cd12699ef41991a896
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-25 21:08:18 +0600

    operators in depth in one file

--- 9.operators.js
+++ 9.operators.js
@@ -4,5 +4,31 @@
 // ## JS operators
 // 1) artihmetic 2) assignment 3) comparison 4)logical 5) bitwise
 
-//--arthmetic are simple math operators
-// except %,**, increment and decrement
+//-- 1) arthmetic are simple math operators
+// except %,**(exponential), increment and decrement
+
+//-- 2)assignment
+// x++ and x+=1 are same, x=x+5 and x+=5 are same and so do
+// -,*,/ and %
+
+// -- 3)comparison
+// Relational >,>=,<=,<
+// Equality ===, !==
+
+// Exceptional -- there is 2 type of equality
+// ( === ) Strict Equality , it checks both the value and type are same or not
+// ( == ) Lose Equality , it checks only the value and converts the type of the left side
+
+// Ternary operator (comparison operator)
+// let points = 110;
+// let status = points > 100 ? "Trusted" : "Trial";
+// console.log(status);
+
+// 4) Logical
+// AND(&&),OR(||),(!)
+
+// Logical operator with non booleans (very important)
+// we can use JS boolean expression out of boolean condition
+// ex. false|| 'imam; // it'll print 'imam'
+// Reason: there exissts some truthy and fals valuse in JS(not true and false)
+// flasy(undefined, null, 0,false,'',NaN)
