83ed100c18b7e9dfa658207b444f2d8fd5ca091c
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-10-26 01:43:13 +0600

    if else and swith case

---
+++ 11.conditional_statements.js
@@ -0,0 +1,14 @@
+// 2 types conditional statements
+// if.....else(if - else if - else if -else)
+// switch...case
+
+// only switch case done because not familiar with it
+let role = "Admin";
+switch (role) {
+  case "Admin":
+    console.log("The Admin");
+    break;
+
+  default:
+    console.log("Guest");
+}
