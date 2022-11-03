d8f696948080ff991dce8f53fcecb9b359f35517
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-03 17:33:01 +0600

    important factory function

--- 15.factroy_functions.js
+++ 15.factroy_functions.js
@@ -1,3 +1,5 @@
+// A factory function can be defined as a function
+// that creates an object and returns it.
 // objective is to creat objects not by hard coding
 // by factory function, we can create many function simply
 function createCircle(radius) {
@@ -11,4 +13,5 @@ function createCircle(radius) {
 
 const circle1 = createCircle(1);
 console.log(circle1);
-const circle2 = createCircle(1);
+const circle2 = createCircle(3);
+console.log(circle2);
