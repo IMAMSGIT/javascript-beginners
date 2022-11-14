76b0524a124261ade4601facf3365f8095c5af3d
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-14 18:47:44 +0600

    functions are objects- need further deep study

--- 19.functions_are_objects.js
+++ 19.functions_are_objects.js
@@ -5,15 +5,19 @@ function Circle(radius) {
   };
 }
 
-const Circle1 = new Function(
-  "radius",
-  ` this.radius = radius;
-this.draw = function () {
-  console.log("Draw");
-};`
-);
+// const Circle1 = new Function(
+//   "radius",
+//   ` this.radius = radius;
+// this.draw = function () {
+//   console.log("Draw");
+// };`
+// );
+
+// ** proof of functions are objects
 
 // when we create a function
 // internally it represents like this
-
-const another = new Circle(1);
+Circle.call({}, 1);
+// exactly same as const another = new Circle(1);
+// if we want to use apply method we have to
+// supply prameters like [1], not just one in the above
