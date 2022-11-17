db7eaab46de03fd534c27ea8d0a0a573f9f11e94
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-17 19:02:19 +0600

    date object's method

---
+++ JS-built-in-objects/date.js
@@ -0,0 +1,9 @@
+//  different method to creat date object
+const now = new Date();
+const date1 = new Date("June 11 2022 09:00");
+const date2 = new Date(2022, 4, 11, 7);
+
+// 3 Date method
+// now.toDateString();
+// now.toTimeString();
+// now.toISOString();
