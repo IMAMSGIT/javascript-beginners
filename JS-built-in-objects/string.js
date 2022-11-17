53c5813260135a553ed349dbc726a9035ebdfc3f
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-17 18:48:49 +0600

    built in string object

---
+++ JS-built-in-objects/string.js
@@ -0,0 +1,7 @@
+const message = "This is a message";
+// above one's type is string
+const another = new String("Another message"); // using constructor function
+//  above one's type is object
+
+const backSlash = "Example of 'backslash";
+const creatsArray = backSlash.split(" ");
