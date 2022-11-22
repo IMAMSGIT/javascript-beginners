aec6c38835f2541ec4f889bff3dc10b5ada236fe
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-22 18:31:46 +0600

    joining arrays - split, join

---
+++ Array/joining_arrays.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3];
+const joined = numbers.join(".");
+console.log(joined);
+// It returns the output as an String
+
+const message = "This is my First message";
+const parts = message.split(" ");
+console.log(parts);
+
+const combined = parts.join("-");
+console.log(combined);
+
+// need to study more later- split method
