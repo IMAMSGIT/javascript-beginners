493bceacaa44080b8f3391a05de6a3d483e6a1c3
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-25 11:51:53 +0600

    moving function declaration on top while executing

---
+++ Function/2.hoisting.js
@@ -0,0 +1,8 @@
+// in Function declaration , we can call it before
+// it is defined
+function walk() {
+  console.log("Walkkk");
+}
+
+// But not possible for function expression
+// Hoisting - moving all the function declaration at the top of the file
