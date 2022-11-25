5190fb6359826dd9867d367038b11b51d01889af
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-11-25 17:24:15 +0600

    arguments default parameter in every function

---
+++ Function/3.arguments.js
@@ -0,0 +1,9 @@
+// we can use function arguments as we like
+
+function sum() {
+  let total = 0;
+  for (let value of arguments) total += value;
+  return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6));
