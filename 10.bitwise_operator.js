5793f2b343ec205e4745a45982c5d9ac2a52c028
IMAMSGIT <imamhosain95@gmail.com>
2023-02-27 15:45:14 +0600

    control flow folder added with basics and bitwise file deleted from outside

--- 10.bitwise_operator.js
+++
@@ -1,7 +0,0 @@
-// this type of operator are not for practical use
-// so discussed separately
-// in decimal
-// 1 is represented as 1 = 00000001
-// 2 is 00000010
-console.log(1 | 2); // add their decimal represented bits, if at least one found, then answer is one
-console.log(1 & 2); // will be one if all are one
---
+++ 4.Control-flow/1.if..else.js
@@ -0,0 +1,4 @@
+let hour = 10;
+if (hour > 6 && hour < 12) console.log("Mornin..");
+else if (hour >= 12 && hour < 18) console.log("Evenin..");
+else console.log("Night");
