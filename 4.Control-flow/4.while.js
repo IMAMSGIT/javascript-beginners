94bef802a8184b5bd43c0a75e855140443fa7ec1
IMAMSGIT <imamhosain95@gmail.com>
2023-02-28 00:14:15 +0600

    while loop to print even numbers

---
+++ 4.Control-flow/4.while.js
@@ -0,0 +1,9 @@
+// Same as for loop
+// But here i is out of loop
+// WE need to declare it externally
+let i = 0;
+while (i < 5) {
+  //   console.log(i);
+  if (i % 2 == 0) console.log(i);
+  i++;
+}
