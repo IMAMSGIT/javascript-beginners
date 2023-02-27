f14366f66d3c2e3f7669539f531e7d404969a93e
IMAMSGIT <imamhosain95@gmail.com>
2023-02-27 23:59:16 +0600

    For loop with printing even numbers

---
+++ 4.Control-flow/3.for_loop.js
@@ -0,0 +1,17 @@
+// List of loops in JS
+// for
+// While
+// Do .. while
+// For...in
+// For...of
+
+for (let i = 10; i > 5; i--)
+  // the loop will run for 10 to 6 as 6>5, not equal
+  // whatever you want to print, your wish
+
+  console.log("hello");
+
+// Print even numbers
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) console.log(i);
+}
