ac8480b9f606af2074383018e91c025d1b7199d1
IMAMSGIT <imamhosain95@gmail.com>
2023-02-28 01:43:13 +0600

    Not as usual to use, old things

---
+++ 4.Control-flow/9.break_continue.js
@@ -0,0 +1,17 @@
+let i = 0;
+while (i < 10) {
+  if (i === 5) break;
+  console.log(i);
+  i++;
+}
+console.log("--------");
+
+let j = 0;
+while (j < 10) {
+  if (j % 2 == 0) {
+    j++;
+    continue;
+  }
+  console.log(j);
+  j++;
+}
