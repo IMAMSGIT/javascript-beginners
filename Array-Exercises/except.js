190d88565904cc7e41a619c71650210971a92a89
IMAMSGIT <imamhosain95@gmail.com>
2023-03-12 01:26:36 +0600

    Be created as new

--- Array-Exercises/except.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1, 2, 3, 4];
-console.log(except(numbers, [2]));
-
-function except(array, excluded) {
-  const output = [];
-  for (let element of array)
-    if (!excluded.includes(element)) output.push(element);
-  return output;
-}
--- Array-Exercises/includes.js
+++
@@ -1,8 +0,0 @@
-const numbers = [1, 2, 4, 5];
-
-console.log(includes(numbers, 3));
-
-function includes(array, searchElement) {
-  for (let element of array) if (element === searchElement) return true;
-  return false;
-}
