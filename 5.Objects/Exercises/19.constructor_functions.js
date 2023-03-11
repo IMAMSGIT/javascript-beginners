7c50df968fc544239837749c42ff5c21fe4ec80c
IMAMSGIT <imamhosain95@gmail.com>
2023-03-12 00:52:50 +0600

    Blog post object by constructor

---
+++ 5.Objects/Exercises/19.constructor_functions.js
@@ -0,0 +1,9 @@
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
+const post = new Post("a", "b", "c");
